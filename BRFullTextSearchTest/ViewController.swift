bed253cc778ce259402933d5f74fa357a8c68081
Adam Gask <adam.gask@myunidays.com>
2016-03-29 16:33:07 +0100

    Initial Commit

---
+++ BRFullTextSearchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BRFullTextSearchTest
+//
+//  Created by Adam Gask on 29/03/2016.
+//  Copyright © 2016 UNiDAYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
